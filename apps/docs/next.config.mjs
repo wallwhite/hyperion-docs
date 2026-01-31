d824c28425a6e4e86f24c9d92954616e50f9b7da
Egor Code <codeegor@gmail.com>
2026-01-31 20:52:29 +0200

    added static mode

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,11 +1,27 @@
 import { createMDX } from 'fumadocs-mdx/next';
+import { SERVER_ENV } from './src/env.ts';
 
 const withMDX = createMDX();
+const isStatic = SERVER_ENV().IS_PROD_STATIC;
+
+if (isStatic) {
+  console.log('Building Docs as Static Export for GitHub Pages\n');
+} else {
+  console.log('Building Docs as Standalone Next.js App\n');
+}
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'standalone',
+  ...(isStatic && {
+    output: 'export',
+    distDir: 'out',
+    images: { unoptimized: true },
+    basePath: '/' + SERVER_ENV().GITHUB_PAGES_REPO_NAME,
+  }),
+  ...(!isStatic && {
+    output: 'standalone',
+  }),
   eslint: {
     // Disable ESLint during production builds
     ignoreDuringBuilds: true,
